215f5cb813b66addadcc17b7d071834a804f6aec
Tank <tengkangfei@hotmail.com>
2018-01-24 22:04:07 +0800

    我来组成头部

--- src/components/head/index.vue
+++ src/components/head/index.vue
@@ -1,11 +1,14 @@
 <template>
   <v-ons-toolbar>
-    <div class="left">
-      <v-ons-toolbar-button @click="toggleSide">
-        <v-ons-icon icon="ion-navicon, material:md-more-vert"></v-ons-icon>
-      </v-ons-toolbar-button>
+
+    <div class="center" style="text-align: center">
+      <div class="left" style="position: fixed">
+        <v-ons-toolbar-button @click="toggleSide">
+          <v-ons-icon icon="ion-navicon"></v-ons-icon>
+        </v-ons-toolbar-button>
+      </div>
+      {{title}}
     </div>
-    <div class="center">{{title}}</div>
   </v-ons-toolbar>
 </template>
 
