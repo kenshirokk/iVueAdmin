af1b5aeb508344b8259c10d3eb2e5912ca7e32cc
shangbansharen <shangbansharen@163.com>
2018-01-25 10:11:59 +0800

    # 格式化

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { getToken,setToken,removeToken } from '@/utils/auth'
+import {getToken, setToken, removeToken} from '@/utils/auth'
 
 Vue.use(Vuex)
 const splitter = {
@@ -9,27 +9,27 @@ const splitter = {
     open: false
   },
   mutations: {
-    toggle (state, shouldOpen) {
+    toggle(state, shouldOpen) {
       if (typeof shouldOpen === 'boolean') {
         state.open = shouldOpen
       } else {
-      state.open = !state.open
-    }
+        state.open = !state.open
+      }
     }
   }
 }
 
 const auth = {
   namespaced: true,
-    state: {
-    loginUser:getToken()?JSON.parse(getToken()):{}
+  state: {
+    loginUser: getToken() ? JSON.parse(getToken()) : {}
   },
   mutations: {
-    setLoginUser(state,loginUser){
+    setLoginUser(state, loginUser) {
       state.loginUser = loginUser
       setToken(loginUser)
     },
-    loginOut(state){
+    loginOut(state) {
       state.loginUser = {}
       removeToken()
     }
