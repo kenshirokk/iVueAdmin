336f296e26f998afecb425352f0087aecdc84f5e
kenshirokk <iamkenshiro@live.cn>
2018-01-26 22:10:15 +0800

    # 最外层元素是 splitter

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,24 +1,21 @@
 <template>
-    <v-ons-page>
-      <v-ons-splitter>
-        <side-menu></side-menu>
-
-        <v-ons-splitter-content>
-          <router-view></router-view>
-        </v-ons-splitter-content>
-      </v-ons-splitter>
-    </v-ons-page>
+  <v-ons-splitter>
+    <side-menu></side-menu>
+    <v-ons-splitter-content>
+      <router-view></router-view>
+    </v-ons-splitter-content>
+  </v-ons-splitter>
 </template>
 
 <script>
-    import SideMenu from '@/components/side-menu'
+  import SideMenu from '@/components/side-menu'
 
-    export default {
-        name: "index",
-        components:{
-          SideMenu
-        }
+  export default {
+    name: "index",
+    components: {
+      SideMenu
     }
+  }
 </script>
 
 <style scoped>
